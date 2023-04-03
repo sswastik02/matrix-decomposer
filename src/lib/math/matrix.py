d227ca1cd7fe32b3fc9b998c7d4511cdf330fd20
Amool-kk <amoolkumar10@gmail.com>
2023-04-03 17:51:17 +0530

    [+] change in decomposition matrix function
    Co-authored-by: nirajraj-13 <nirajraj-13@users.noreply.github.com>

--- src/lib/math/matrix.py
+++ src/lib/math/matrix.py
@@ -1,37 +1,61 @@
 import numpy as np
 
-def lDecomposition(matrix):
+def uDecomposition(matrix):
     n = len(matrix)
     lower = np.zeros((n, n))
+    upper = np.zeros((n, n))
  
-    # Lower Triangular
-    for j in range(i, n):
-        if i == j:
-            lower[i][i] = 1  # Diagonal as 1
-        else:
-            sum = 0
-            for k in range(i):
-                sum += (lower[j][k] * upper[k][i])
-            lower[j][i] = (matrix[j][i] - sum) / upper[i][i]
- 
-    return upper
-def uDecomposition(matrix):
-    n= len(matrix)
-    upper = np.zeros((n,n))
-    
+    # Decomposition
     for i in range(n):
+        # Upper Triangular
         for j in range(i, n):
             sum = 0
             for k in range(i):
-                sum += (lower[i][k]*upper[k][j])
+                sum += (lower[i][k] * upper[k][j])
             upper[i][j] = matrix[i][j] - sum
+            
+        for j in range(i, n):
+            if i == j:
+                lower[i][i] = 1  # Diagonal as 1
+            else:
+                sum = 0
+                for k in range(i):
+                    sum += (lower[j][k] * upper[k][i])
+                lower[j][i] = (matrix[j][i] - sum) / upper[i][i]
+ 
     return upper
-    
+
+
+def lDecomposition(matrix):
+    n = len(matrix)
+    lower = np.zeros((n, n))
+    upper = np.zeros((n, n))
+ 
+    # Decomposition
+    for i in range(n):
+        # Lower Triangular
+        for j in range(i, n):
+            sum = 0
+            for k in range(i):
+                sum += (lower[i][k] * upper[k][j])
+            upper[i][j] = matrix[i][j] - sum
+ 
+        for j in range(i, n):
+            if i == j:
+                lower[i][i] = 1  # Diagonal as 1
+            else:
+                sum = 0
+                for k in range(i):
+                    sum += (lower[j][k] * upper[k][i])
+                lower[j][i] = (matrix[j][i] - sum) / upper[i][i]
+ 
+    return lower
  
 matrix = np.array([[2, -1, -2], [-4, 6, 3], [-4, -2, 8]])
-n = len(matrix)
 lower = lDecomposition(matrix)
 upper = uDecomposition(matrix)
+
+n = len(matrix)
 
 print("Lower Triangular\t\tUpper Triangular")
 for i in range(n):
